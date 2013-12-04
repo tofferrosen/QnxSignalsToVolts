6f471e5d9c81f75f2e7f20e7f66fe9ccad3ce462
unknown <cbr4830@rit.edu>
2013-12-04 16:04:46 -0500

    More comments, removed unnecssary defines

--- Project6.cc
+++ Project6.cc
@@ -1,33 +1,18 @@
-#include <cstdlib>
-#include <iostream>
-#include <Converter.h>
-
-
 /*
  * Project 6
- * Receives input from the QNX Digital I/O board from a signal generator.
- * Converts the input into voltage, converts this value to corresponding servo motor
- * position and sends it to Freescale.
+ * Implements a stand-alone system to provide a rough inidcation
+ * of voltage from a waveform created by a signal generator using
+ * servo motors
  *
- * The main loop will consistently check the input pin, send the input to the converter class, and wait
- * for the voltage result. It will then convert this value to a servos motor positon and send it to freescale.
+ * QNX implementation
  *
  * @author Christoffer Rosen
  * @author Lennard Streat
  */
 
-/* Port Direction Register */
-#define PORT_DIR_OFFSET (11)
-#define PORT_DIR_ADDR (BASE_ADDR + PORT_DIR_OFFSET)
-
-/* Set port for input */
-#define DIOIN_PORT (0b1111111) // all input
-
-/* Define PORTA address */
-#define PORTA_OFFSET (8)
-#define PORTA_ADDR (BASE_ADDR + PORTA_OFFSET)
-
-#define A0BIT (0b11111110) // A mask used to access the 0 bit of Port A
+#include <cstdlib>
+#include <iostream>
+#include <Converter.h>
 
 int main(int argc, char *argv[]) {
 
@@ -46,13 +31,11 @@ int main(int argc, char *argv[]) {
 		Converter * converter = new Converter();
 		converter->initalize();
 
+		/* Start program */
 		while(true){
 			converter->convert();
 			std::cout << "Reading.. " << converter->getVoltage() << "\n";
 		}
-
-
-
 		return EXIT_SUCCESS;
 	}
 }
